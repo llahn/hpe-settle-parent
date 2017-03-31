7d60d786069f6ccfc5a4256a7a59bd36bc177b66
lei.li3 <lei.li3@hpe.com>
2017-03-31 10:12:04 +0800

    asdasd

---
+++ hpe-settle-demo/src/main/java/com/hpe/settle/demo/controller/AsyncController.java
@@ -0,0 +1,37 @@
+package com.hpe.settle.demo.controller;
+
+import java.util.concurrent.Future;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hpe.settle.demo.async.AsyncTask;
+
+@RestController
+public class AsyncController {
+
+	@Autowired
+	private AsyncTask task;
+
+	@RequestMapping("/async")
+	public void async() throws Exception {
+		long start = System.currentTimeMillis();
+
+		Future<String> task1 = task.doTaskOne();
+		Future<String> task2 = task.doTaskTwo();
+		Future<String> task3 = task.doTaskThree();
+
+		while (true) {
+			if (task1.isDone() && task2.isDone() && task3.isDone()) {
+				// 三个任务都调用完成，退出循环等待
+				break;
+			}
+			Thread.sleep(1000);
+		}
+
+		long end = System.currentTimeMillis();
+
+		System.out.println("任务全部完成，总耗时：" + (end - start) + "毫秒");
+	}
+}
